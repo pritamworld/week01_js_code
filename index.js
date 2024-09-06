bb8109f452a7727a53ed7b4e40a7ea3352d9229f
moxDroid Labs Inc <pritam.world@gmail.com>
2024-09-06 19:38:37 -0400

    js code init

---
+++ index.js
@@ -0,0 +1,57 @@
+console.log('Hello, world!');
+
+a = 10
+var a = 10
+// let a = 10
+
+let b = 20
+b= 100
+b = "Hello"
+
+// var b = 
+
+//Object Literal
+var student = {
+    100: 'Hundreds',
+    "student Id": 1,
+    studentName: "John",
+    studentAge: 20,
+    city: "New York"
+}
+console.log(student)
+console.log(student.studentName)
+console.log(student[100])
+console.log(student["student Id"])
+console.log(typeof student)
+
+for(let key in student){
+    console.log(`${key} -> ${student[key]}`)
+}
+//
+
+var emp = {
+    empId: 1,
+    empName: "John",
+    empAge: 20,
+    empCity: "New York"
+}
+//Object Destructuring
+var {empId, empName, empAge, empCity} = emp
+var result = "Pass"
+console.log(empId)
+
+var newEmp = {
+    empId,
+    empName,
+    empAge,
+    empCity,
+    result
+}
+
+console.log(newEmp)
+
+var emp1 = {
+    ...emp,
+    result
+}
+console.log(emp1)
